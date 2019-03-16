dfb202bda8e67790b5aa8f30e808dac753e92424
Devin Kapla <devinkapla@gmail.com>
2019-03-16 17:02:42 -0600

    Finsih fetch api calls

--- src/__test__/Api.test.js
+++ src/__test__/Api.test.js
@@ -1,58 +1,112 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Api, { fetchByKey } from '../utilities/Api';
+import Api, { fetchByKey, peoplePull, planetsPull, vehiclesPull } from '../utilities/Api';
 
 describe('Api', () => {
 	let mockData;
-
-	beforeEach(() => {
-		mockData = {
-			name: "Tatooine"
-		}
-
-		fetch = jest.fn().mockImplementation(() => Promise.resolve({
-			ok: true,
-			status: 200,
-			json: () => Promise.resolve(mockData),
-		}));
-
-	});
-
-	it("fetch call takes expected key", () => {
-		// setup
-		const key = 'dinosuars'
-		// execution
-		fetchByKey(key);
-		// expectation
-		expect(fetch).toHaveBeenCalledWith(`https://swapi.co/api/dinosuars`);
-	});
-
-	it("fetch call returns expected data", () => {
-		// setup
-		const key = 'dinosuars'
-		// execution
-		fetchByKey(key)
-			.then(data => {
-				// expectation
-				expect(data).toEqual(mockData);
+	let mockValues;
+	let mockFetchValues;
+	describe('fetchByKey', () => {
+		beforeEach(() => {
+			mockData = {
+				name: "Tatooine"
+			}
+			fetch = jest.fn().mockImplementation(() => Promise.resolve({
+				ok: true,
+				status: 200,
+				json: () => Promise.resolve(mockData),
+			}));
+		});
+	
+		it("fetch call takes expected key", () => {
+			// setup
+			const key = 'dinosuars'
+			// execution
+			fetchByKey(key);
+			// expectation
+			expect(fetch).toHaveBeenCalledWith(`https://swapi.co/api/dinosuars`);
+		});
+	
+		it("fetch call returns expected data", () => {
+			// setup
+			const key = 'dinosuars'
+			// execution
+			fetchByKey(key)
+				.then(data => {
+					// expectation
+					expect(data).toEqual(mockData);
+				})
+		});
+		// it("if response not ok show error", () => {
+		// 	// setup
+		// 	const key = 'dinosuars';
+	
+		// 	fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
+		// 		ok: false
+		// 	}));
+		// 	// execution
+		// 	fetchByKey(key)
+		// 		.catch(error => {
+		// 			// expectation
+		// 			// console.log(error.message)
+		// 			expect(error.message).toBe('Response not ok');
+		// 		})
+	
+		// });
+	})
+	describe('peoplePull', () => {
+		beforeEach(() => {
+			mockValues = [
+				{
+					name: "David",
+					homeworld: 'https://swapi.co/api/planet0',
+					species: 'https://swapi.co/api/species0',
+					forceMaster: true
+				},
+				{
+					name: "Devin",
+					homeworld: 'https://swapi.co/api/planet1',
+					species: 'https://swapi.co/api/species1',
+					forceMaster: true
+				},
+				{
+					name: "Christie",
+					homeworld: 'https://swapi.co/api/planet2',
+					species: 'https://swapi.co/api/species2',
+					forceMaster: false
+				}
+			]
+			// mockPerson = {
+			// 	name: "David",
+			// 	homeworld: 'https://swapi.co/api/planet0',
+			// 	species: 'https://swapi.co/api/species0'
+			// }
+			fetch = jest.fn().mockImplementation(() => Promise.resolve({
+				ok: true,
+				status: 200
+			}));
+			mockFetchValues = jest.fn()
+		});
+		it('should fetch by specific key for each value', () => {
+			// setup
+			// execution
+			peoplePull(mockValues)
+			// expectation
+			mockValues.forEach((val, i) => {
+				expect(fetch).toHaveBeenCalledWith(`https://swapi.co/api/planet${i}`)
 			})
-	});
-
-	it("if response not ok show error", () => {
-		// setup
-		const key = 'dinosuars';
-
-		fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
-			ok: false
-		}));
-		// execution
-		fetchByKey(key)
-			.catch(error => {
-				// expectation
-				// console.log(error.message)
-				expect(error.message).toBe('Response not ok');
+			// expect(fetch).toHaveBeenCalledWith('https://swapi.co/api/planet1')
+			// expect(fetch).toHaveBeenCalledWith('https://swapi.co/api/planet2')
+		})
+		it('should call fetchValues with species', () => {
+			// setup
+			// execution
+			peoplePull(mockValues)
+			// expectation
+			mockValues.forEach((val, i) => {
+				expect(mockFetchValues).toHaveBeenCalledWith(`https://swapi.co/api/species${i}`)
 			})
-
-	});
-
+			
+		})
+	})
 })
--- src/utilities/Api.js
+++ src/utilities/Api.js
@@ -13,46 +13,53 @@ const peoplePull = (values) => {
 			.then(data => ({ ...peep, 
 											 homeworld: data.name,
 											 population: data.population}))
-			.then(dataTwo => fetchSingle(species[0], 'name', dataTwo))
+			.then(dataTwo => {
+				return fetchValues(species)
+					.then(val => {
+						const species = val.join('')
+						return {...dataTwo, species}})
+			})
 	})
 	return Promise.all(cleanedPeeps)
 }
 
+// const fetchSpecies = () => {
+
+// }
+
 const planetsPull = (values) => {
-	console.log(values)
 	const cleanedPlanets = values.map(elem => {
 		const { name, terrain, population, climate, residents } = elem
-		const peeps = fetchValues(residents)
-		return { name, terrain, population, climate, favorite: false, peeps }
+		return fetchValues(residents)
+			.then(data => {
+				let residents = data.join(', ')
+				return { name, terrain, population, climate, favorite: false, residents }})
 	})
 	return Promise.all(cleanedPlanets)
-	
-	// Name
-	// Terrain
-	// Population
-	// Climate
-	// Residents
-	// A button to “Favorite” the planet
 }
 
 const fetchValues = (values) => {
 	const cleanedValues = values.map(val => {
-		
+		return fetch(val)
+			.then(response => response.json())
+			.then(data => data.name)
 	})
-	return fetch(values)
-		.then(response => response.json())
-		.then(data => data.name)
-		.then(name => name)
+	return Promise.all(cleanedValues)
+
 }
 const vehiclesPull = (values) => {
-	console.log(values)
+	const cleanedVehicles = values.map(elem => {
+		const { name, model, vehicle_class, passengers } = elem
+		return { name, model, class: vehicle_class, passengers, favorite: false }
+	})
+	return Promise.all(cleanedVehicles)
 }
 
-const fetchSingle = (url, key, data) => {
-	return fetch(url)
-		.then(response => response.json())
-		.then(data => data[key])
-		.then(result => ({ ...data, extra: result }))
-}
+// const fetchSingle = (url, key, data) => {
+// 	return fetch(url)
+// 		.then(response => response.json())
+// 		.then(data => data[key])
+// 		.then(result => ({ ...data, extra: result }))
+// }
 
 export { fetchByKey, peoplePull, planetsPull, vehiclesPull }
